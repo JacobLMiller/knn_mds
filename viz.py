48c6b3af62305ed7225acf8cf48393f776f792de
Jacob Lee Miller <jacobmiller1@cs.arizona.edu>
2022-03-24 18:19:27 -0700

    Adding drawings,figures.

--- viz.py
+++ viz.py
@@ -61,10 +61,54 @@ def custom2():
         y = (sum( ((x[2*i]-x[2*j])**2, (x[2*i+1]-x[2*j+1])**2) )) ** 0.5
 
 
+norm = np.linalg.norm
+import itertools
+#@jit(nopython=True,cache=True)
 def full_norm(n):
-    import itertools
-    indices = list(itertools.combinations(range(n), 2))
     shuffle = np.random.shuffle
+    randint = random.randint
+    indices = list(itertools.combinations(range(n), 2))
+
+    X = np.random.uniform(0,1,(n,2))
+    d = np.random.uniform(0,1,(n,n))
+    w = np.ones(d.shape)
+    step = 0.001
+
+    for epoch in range(30):
+
+        change = 10
+
+        for i,j in indices:
+
+            pq = X[i]-X[j]
+            mag = norm(pq)
+            mag_grad = pq/mag
+
+
+            mu = step*w[i][j]
+            if mu >= 1: mu = 1
+
+            r = (mu*(mag-d[i][j]))/(2*mag)
+            stress = r*pq
+
+            mu1 = step if step < 1 else 1
+            repulsion = -((step/mag) * mag_grad)
+
+            l_sum = 1+0.01
+            m = (1/l_sum)*stress + (0.01/l_sum)*repulsion
+
+            X[i] -= m
+            X[j] += m
+
+        shuffle(indices)
+
+from math import sqrt
+#@jit(nopython=True,cache=True)
+def full_custom(n):
+
+    shuffle = np.random.shuffle
+    randint = random.randint
+    indices = list(itertools.combinations(range(n), 2))
 
     X = np.random.uniform(0,1,(n,2))
     d = np.random.uniform(0,1,(n,n))
@@ -75,60 +119,82 @@ def full_norm(n):
         change = 10
 
         for i,j in indices:
+
             #old = np.linalg.norm(X[i]-X[j])
-            pq = X[i]-X[j]
-            #mag = (pq[0]*pq[0] + pq[1]*pq[1]) ** 0.5
-            mag = norm(pq)
-            mag_grad = pq/mag
+            #pq = X[i]-X[j]
+            #square = pq ** 2
+            mag = ( (X[i][0]-X[j][0])**2 + (X[i][1]-X[j][1])**2) ** 0.5
+            #mag = norm(pq)
+            #mag_grad = pq/mag
+            dx = (X[i][0]-X[j][0])/mag
+            dy = (X[i][1]-X[j][1])/mag
 
             #w = 1/(dij**2)
             mu = step*w[i][j]
             if mu >= 1: mu = 1
 
             r = (mu*(mag-d[i][j]))/(2*mag)
-            stress = r*pq
+            stressx = r*dx
+            stressy = r*dy
 
             mu1 = step if step < 1 else 1
-            repulsion = -((step/mag) * mag_grad)
-            # if mag > 3*diam:
-            #     repulsion *= 1
-            #
+            repulsionx = -((step/mag) * dx)
+            repulsiony = -((step/mag) * dy)
+
             l_sum = 1+0.01
-            m = (1/l_sum)*stress + (0.01/l_sum)*repulsion
+            mx = (1/l_sum)*stressx + (0.01/l_sum)*repulsionx
+            my = (1/l_sum)*stressy + (0.01/l_sum)*repulsiony
 
-            X[i] -= m
-            X[j] += m
+            X[i][0] -= mx
+            X[i][1] -= my
+            X[j][0] += mx
+            X[j][1] += my
 
-        #shuffle(indices)
-
-import time
-sizes = list(range(5,100,10))
-np_m = np.zeros(len(sizes))
-cust = np.zeros(len(sizes))
-
-iter = 1
-
-for n in range(len(sizes)):
-    for i in range(iter):
-        start = time.perf_counter()
-        full_norm(sizes[n])
-        end = time.perf_counter()
-        np_m[n] += end-start
-
-        start = time.perf_counter()
-        np_norm(sizes[n])
-        end = time.perf_counter()
-        cust[n] += end-start
-
-cust /= iter
-np_m /= iter
+        shuffle(indices)
 
 
-import matplotlib.pyplot as plt
-plt.plot(sizes[1:],np_m[1:],label='numpy')
-plt.plot(sizes[1:],cust[1:],label='custom')
-plt.legend()
-plt.show()
 
-import cProfile
-cProfile.run('full_norm(25)')
+
+def timing(n):
+    setup = '''
+import numpy as np
+from __main__ import full_custom
+    '''
+
+    loop = '''
+full_custom(n={})
+    '''.format(n)
+
+    number = 10
+    import timeit
+    hit = timeit.timeit(setup=setup,
+                        stmt=loop,number=number)
+    print(hit/number)
+    return hit/number
+
+def timing2(n):
+    setup = '''
+import numpy as np
+from __main__ import full_norm
+    '''
+
+    loop = '''
+full_norm(n={})
+    '''.format(n)
+
+    number = 100
+    import timeit
+    hit = timeit.timeit(setup=setup,
+                        stmt=loop,number=number)
+    print(hit/number)
+    return hit/number
+
+if __name__ == "__main__":
+    toplot = [timing(n) for n in range(10,1000,100)]
+    toplot2 = [timing2(n) for n in range(10,1000,100)]
+    import matplotlib.pyplot as plt
+    plt.suptitle("with jit")
+    plt.plot(list(range(10,1000,100)), toplot,label='custom')
+    plt.plot(list(range(10,1000,100)), toplot2,label='numpy')
+    plt.legend()
+    plt.show()
